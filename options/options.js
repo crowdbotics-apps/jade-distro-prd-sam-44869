eaf4e224b0b8491bedcaf05ce15f7e782f8f793e
Crowdbotics <team@crowdbotics.com>
2023-12-05 18:07:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jade-distro-prd-sam-44869",
+  url: "https://jade-distro-prd-sam-44869.botics.co",
+  api: "https://jade-distro-prd-sam-44869.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
